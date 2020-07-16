52e7eec26c9f77fad9202f43d83d2cab21b91e99
hadla <hadla.berman@gmail.com>
2020-07-16 16:24:48 +0200

    Created Carousel component to display images at the login page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import image from './images/image.jpg'
 
 import SignUpForm from './pages/SignUpForm';
 import SignInForm from './pages/SignInForm';
+import Carousel from './components/Carousel';
 import './App.css';
 
 function App() {
   return (
     <Router basename='/moments/'>
       <div className='App'>
-        <div className='App__Aside'></div>
+        <div className='App__Aside'>
+          <Carousel />
+        </div>
         <div className='App__Form'>
           <div className='PageSwitcher'>
             <NavLink to='/sign-in' activeClassName='PageSwitcher__Item--Active' className='PageSwitcher__Item'>
