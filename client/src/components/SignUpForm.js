a31e487f463c37b1b49969eef6023dccdfc6c2b9
Fred Abrahamson <fredabrahamson@Freds-MBP-2.lan>
2020-07-22 18:02:07 +0200

    Removed nested routers

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import CheckboxAlert from './CheckboxAlert';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import CheckboxAlert from "./CheckboxAlert";
 
 class SignUpForm extends Component {
   constructor() {
     super();
 
     this.state = {
-      email: '',
-      password: '',
-      name: '',
+      email: "",
+      password: "",
+      name: "",
       hasAgreed: false,
     };
 
@@ -19,7 +19,7 @@ class SignUpForm extends Component {
 
   handleChange(e) {
     let target = e.target;
-    let value = target.type === 'checkbox' ? target.checked : target.value;
+    let value = target.type === "checkbox" ? target.checked : target.value;
     let name = target.name;
 
     this.setState({
@@ -30,74 +30,75 @@ class SignUpForm extends Component {
   handleSubmit(e) {
     e.preventDefault();
 
-    console.log('The form was submitted with the following data:');
+    console.log("The form was submitted with the following data:");
     console.log(this.state);
   }
 
   render() {
     console.log(this.props);
     return (
-      <div className='FormCenter'>
-        <form onSubmit={this.handleSubmit} className='FormFields'>
-          <div className='FormField'>
-            <label className='FormField__Label' htmlFor='name'>
+      <div className="FormCenter">
+        <form onSubmit={this.handleSubmit} className="FormFields">
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="name">
               Full Name
             </label>
             <input
-              type='text'
-              id='name'
-              className='FormField__Input'
-              placeholder='Enter your full name'
-              name='name'
+              type="text"
+              id="name"
+              className="FormField__Input"
+              placeholder="Enter your full name"
+              name="name"
               value={this.state.name}
               onChange={this.handleChange}
             />
           </div>
-          <div className='FormField'>
-            <label className='FormField__Label' htmlFor='password'>
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="password">
               Password
             </label>
             <input
-              type='password'
-              id='password'
-              className='FormField__Input'
-              placeholder='Enter your password'
-              name='password'
+              type="password"
+              id="password"
+              className="FormField__Input"
+              placeholder="Enter your password"
+              name="password"
               value={this.state.password}
               onChange={this.handleChange}
             />
           </div>
-          <div className='FormField'>
-            <label className='FormField__Label' htmlFor='email'>
+          <div className="FormField">
+            <label className="FormField__Label" htmlFor="email">
               E-Mail Address
             </label>
             <input
-              type='email'
-              id='email'
-              className='FormField__Input'
-              placeholder='Enter your email'
-              name='email'
+              type="email"
+              id="email"
+              className="FormField__Input"
+              placeholder="Enter your email"
+              name="email"
               value={this.state.email}
               onChange={this.handleChange}
             />
           </div>
 
-          <div className='FormField'>
-            <label className='FormField__CheckboxLabel'>
+          <div className="FormField">
+            <label className="FormField__CheckboxLabel">
               <input
-                className='FormField__Checkbox'
-                type='checkbox'
-                name='hasAgreed'
+                className="FormField__Checkbox"
+                type="checkbox"
+                name="hasAgreed"
                 value={this.state.hasAgreed}
                 onChange={this.handleChange}
-              />{' '}
-              I agree all statements in <CheckboxAlert className='FormField__TermsLink'></CheckboxAlert>
+              />{" "}
+              I agree all statements in{" "}
+              <CheckboxAlert className="FormField__TermsLink"></CheckboxAlert>
             </label>
           </div>
 
-          <div className='FormField'>
-            <button className='FormField__Button mr-20'>Sign Up</button>{' '}
-            <Link to='/sign-in' className='FormField__Link'>
+          <div className="FormField">
+            <button className="FormField__Button mr-20">Sign Up</button>{" "}
+            <Link to="/sign-in" className="FormField__Link">
               I'm already member
             </Link>
           </div>
