8b9c5c216ab33b58ed54a924d06565ecc3150519
Hadla Bergman <hadla.bergman@appliedtechnology.se>
2020-07-24 16:21:02 +0200

    Finished styling menu and Sidebar component.

--- client/src/components/SidebarContent.js
+++ client/src/components/SidebarContent.js
@@ -3,6 +3,8 @@ import '../style/sidebarContent.css';
 import profileImnage from '../images/profile-image.jpg';
 import SettingsIcon from '@material-ui/icons/Settings';
 import MomentsIcon from '@material-ui/icons/PhotoLibrary';
+import AddMomentIcon from '@material-ui/icons/AddAPhoto';
+import ShareIcon from '@material-ui/icons/Share';
 
 class sidebarContent extends React.Component {
   render() {
@@ -17,10 +19,18 @@ class sidebarContent extends React.Component {
           </div>
         </div>
         <div className='sidebar-menu'>
-          <h4>Menu</h4>
+          <h3 className='menu-title'>Menu</h3>
+          <div className='menu-item'>
+            <AddMomentIcon fontSize='small' className='menu-icon' />
+            <p className='menu-text'>Add Moment</p>
+          </div>
           <div className='menu-item'>
             <MomentsIcon fontSize='small' className='menu-icon' />
             <p className='menu-text'>My Moments</p>
+          </div>
+          <div className='menu-item'>
+            <ShareIcon fontSize='small' className='menu-icon' />
+            <p className='menu-text'>Shared Moments</p>
           </div>
           <div className='menu-item'>
             <SettingsIcon fontSize='small' className='menu-icon' />
