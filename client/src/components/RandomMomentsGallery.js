a0ae228544419032010ab74198a2927d38559340
Hadla Bergman <hadla.bergman@appliedtechnology.se>
2020-08-11 01:33:03 +0200

    Created RandomMomentsGallery to display a random moment from random moment colletions. Started on style, next step is functionality.

---
+++ client/src/components/RandomMomentsGallery.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+import { Link } from 'react-router-dom';
+import '../style/randomMomentsGallery.css';
+
+class RandomMomentsGallery extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //    this.state = {
+  //     };
+  // }
+
+  randomImgDisplayer(image) {
+    return (
+      <div className='RMG-card'>
+        <div className='RMG-img-container'>
+          <img className='RMG-img' src={image.momentsImg} />
+        </div>
+        <p className='RMG-col-name'>From {image.colName}</p>
+      </div>
+    );
+  }
+
+  render() {
+    return <div className='RMG-card-containter'>{this.props.images.map(this.randomImgDisplayer)}</div>;
+  }
+}
+export default RandomMomentsGallery;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,70 @@ import { LinearProgress } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import firebaseApp from '../firebase';
 import '../style/home.css';
+import RandomMomentsGallery from '../components/RandomMomentsGallery';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
+      collections: [],
     };
+    this.testCol = [
+      {
+        momentsImg:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQ-nZ-0Z7C_UGq1UK3yDMuvCdvjSkfUGtGPg&usqp=CAU',
+        colName: 'Family',
+      },
+      {
+        momentsImg: 'https://cdn3.eyeem.com/thumb/5245a9c18f8356cc6f14ee8d2361cdade03a9929-1582881296050/w/1300',
+        colName: 'Friends',
+      },
+      {
+        momentsImg: 'https://i.pinimg.com/originals/60/dd/bd/60ddbd5d5ee63d765116f41110885611.jpg',
+        colName: 'New York',
+      },
+      {
+        momentsImg:
+          'https://images.squarespace-cdn.com/content/v1/5672f59f1115e0e8174b184a/1471617595297-1Q56XNYY6HDHBPG2J9B5/ke17ZwdGBToddI8pDm48kFWxnDtCdRm2WA9rXcwtIYR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcTSrQkGwCGRqSxozz07hWZrYGYYH8sg4qn8Lpf9k1pYMHPsat2_S1jaQY3SwdyaXg/advertising-children-photographer.jpg?format=2500w',
+        colName: 'Family',
+      },
+      {
+        momentsImg: 'https://www.thewanderinglens.com/wp-content/uploads/2018/01/header.jpg',
+        colName: 'Paris',
+      },
+      {
+        momentsImg:
+          'https://images.squarespace-cdn.com/content/v1/5e4a625c50c5eb630b9ca41c/1584010145039-PCQASJO9VP8YYZE5ABA9/ke17ZwdGBToddI8pDm48kIQg3raHUQ0nUnT-YQ2iwWt7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmTl_ALRZE0UkEheIF40jl8o7Lf57f4boaChwgQgKiLE_IR9WP6Dycs2VQZ0vSmXZR/barnfotograf-children-photography-juliana-wiklund-357.jpg',
+        colName: 'Family',
+      },
+      {
+        momentsImg:
+          'https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2019/01/fstoppers_underwater_photography_etiquette_joanna_lentini_r1a3753.jpg',
+        colName: 'Costa Rica',
+      },
+      {
+        momentsImg: 'https://i.pinimg.com/originals/41/e1/6b/41e16b152707dea9c4dfac5e3c3290e8.jpg',
+        colName: 'Summer 2019',
+      },
+      {
+        momentsImg: 'https://shutupandgo.travel/app/uploads/2019/04/Balloonbs.jpg',
+        colName: 'Summer 2019',
+      },
+      {
+        momentsImg:
+          'https://cdn-7.nikon-cdn.com/Images/Learn-Explore/Photography-Techniques/2016/Graduation-photo-tips/Media/Graduation-photos-2-girlfriends.jpg',
+        colName: 'Family',
+      },
+      {
+        momentsImg: 'https://daily.jstor.org/wp-content/uploads/2015/06/Kindergarten.jpg',
+        colName: 'Sofie',
+      },
+      {
+        momentsImg:
+          'https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80',
+        colName: 'Summer 2019',
+      },
+    ];
   }
 
   componentDidMount() {
@@ -36,6 +93,9 @@ class Home extends React.Component {
           <div className='welcome-header'>
             <p className='welcome-msg'>Welcome {this.state.name}!</p>
             <button className='welcome-msg-btn'>Let's get started</button>
+          </div>
+          <div>
+            <RandomMomentsGallery images={this.testCol} />
           </div>
         </div>
       </div>
